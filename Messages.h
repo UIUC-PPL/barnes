dab9e9789f2e37856ab03b0a7268500555b63e30
Pritish Jetley <pjetley2@illinois.edu>
2011-10-23 17:38:19 -0500

    doc for messages

--- Messages.h
+++ Messages.h
@@ -1,27 +1,52 @@
 #ifndef __MESSAGES_H__
 #define __MESSAGES_H__
 
+/*
+ * CharmBH - Messages.h
+ * Definitions of message structures used for various tasks.
+ */
+
 #include "Particle.h"
 #include "MultipoleMoments.h"
 #include "Node.h" 
 
+/*
+ * RangeMsg:
+ * During decomposition, the master DataManager (DM) communicates the range 
+ * of particles held by each tree piece to all other DMs. It uses the RangeMsg
+ * for this purpose.
+ */
 struct RangeMsg : public CMessage_RangeMsg {
   Key *keys;
   int numTreePieces;
 };
 
+/*
+ * When a DM requests a bucket of particles from another, it receives a 
+ * ParticleReplyMsg in return.
+ */
 struct ParticleReplyMsg : public CMessage_ParticleReplyMsg {
   Key key;
   ExternalParticle *data;
   int np;
 };
 
+/*
+ * When a DM requests the subtree under a particular node from another DM, 
+ * it receives a NodeReplyMsg in return.
+ */
 struct NodeReplyMsg : public CMessage_NodeReplyMsg {
   Key key;
   Node<ForceData> *data;
   int nn;
 };
 
+/* 
+ * In order to be responsive to requests for data from remote DMs,
+ * a TreePiece must cooperatively relinquish the processor after doing some
+ * traversal work. In order to reschedule traversal work, the TreePiece
+ * sends a RescheduleMsg to itself.
+ */
 struct RescheduleMsg : public CMessage_RescheduleMsg {
 };
 #endif
