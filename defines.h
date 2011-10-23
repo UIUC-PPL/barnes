cb5d8a19124c45838ac70714055be1c86c94d65e
Pritish Jetley <pjetley2@illinois.edu>
2011-10-23 17:02:45 -0500

    cleanup and document defines.h and defaults.h

--- defines.h
+++ defines.h
@@ -1,39 +1,58 @@
 #ifndef __DEFINES_H__
 #define __DEFINES_H__
 
+/*
+ * CharmBH: defines.h
+ * Definitions of a number of constants used in the simulation code.
+ */
+
 #include "limits.h"
 #include "charm++.h"
-
 #include "common.h"
 
-
+// Number of bits needed for a node or particle's Key (defaults to 64) 
 #define TREE_KEY_BITS (sizeof(Key)*CHAR_BIT)
 
+/*
+ * When the floating point coordinates of a particle are integerized, 
+ * there are BITS_PER_DIM bits available (out of a total of TREE_KEY_BITS)
+ * to hold the integer equivalent of each floating point coordinate.
+ */
 #define BITS_PER_DIM 21
+/*
+ * Therefore, each integer coordinate of a particle's position can take one
+ * of BOXES_PER_DIM values. 
+ */
 #define BOXES_PER_DIM (1<<(BITS_PER_DIM))
 
 #define NDIMS 3
 
+/*
+ * When doing decomposition or tree building, we allow leaves to have up to
+ * *_TOLERANCE times the number of particles specfied by user (through -ppc for 
+ * decomposition and -b for tree building)
+ */
 #define DECOMP_TOLERANCE 1.2
 #define BUCKET_TOLERANCE 1.2
 
+/*
+ * Quick way to find out whether an integer is even. Avoids the '%' operation.
+ */
 #define DIV2(x) ((x)>>1)
 #define EVEN(x) ((DIV2(x)<<1)==(x))
 
+/*
+ * When constructing a tree, each node is either a leaf or has BRANCH_FACTOR children.
+ */
 #define BRANCH_FACTOR 2
 #define LOG_BRANCH_FACTOR 1
 
-const double opening_geometry_factor = 2 / sqrt(3.0);
-
-#define COSMO_CONST(x) (x)
-#define CONVERT_TO_COSMO_TYPE
-
-//#define RRDEBUG CkPrintf
 #define RRDEBUG /* empty */
-
-//#define TB_DEBUG CkPrintf
 #define TB_DEBUG /* empty */
 
+/*
+ * To assign priorities to different tasks.
+ */
 #define NUM_PRIORITY_BITS (sizeof(int)*CHAR_BIT)
 
 #define REQUEST_MOMENTS_PRIORITY (-8)
@@ -44,8 +63,5 @@ const double opening_geometry_factor = 2 / sqrt(3.0);
 #define RECV_PARTICLES_PRIORITY (-3)
 #define REMOTE_GRAVITY_PRIORITY (-2)
 #define LOCAL_GRAVITY_PRIORITY (-1)
-
-#define REMOTE_NODE_REQUEST 9998
-#define REMOTE_PARTICLE_REQUEST 9999
 
 #endif
