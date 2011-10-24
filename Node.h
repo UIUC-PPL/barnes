ab3c1e9efef637b8e673e010770388fca8a2c937
Pritish Jetley <pjetley2@illinois.edu>
2011-10-23 22:40:40 -0500

    Node.h comments

--- Node.h
+++ Node.h
@@ -1,6 +1,18 @@
 #ifndef __TREE_NODE_H__
 #define __TREE_NODE_H__
 
+/*
+ * CharmBH: Node.h
+ * Tree node data structure. Used in spatial partitioning of particles.
+ * Each node has a key, type, depth, pointer to the particles underneath
+ * it (valid only for "Internal" nodes, i.e. those nodes for which all
+ * enclosed particles are hosted on the same PE as the node itself.), 
+ * and child nodes. 
+ * The Node data structure also tracks the owners of a node, i.e. 
+ * which TreePieces have particles under that node. So, for example,
+ * the root is owned collectively by all the TreePieces that have particles.
+ */
+
 #include "MultipoleMoments.h"
 #include "OrientedBox.h"
 #include "Particle.h"
@@ -10,17 +22,32 @@
 #include <stack>
 using namespace std;
 
+// Type of node
 enum NodeType {
   Invalid = 0,
 
+  // All particles under the node are on this PE
   Internal,
+  // This is a leaf
   Bucket,
+  // This is a leaf with no particles in it
   EmptyBucket,
 
+  /* 
+   * Some of the particles underneath this node are
+   * owned by TreePieces that are not on this PE
+  */
   Boundary,
 
+  /*
+   * None of the particles underneath this node are owned 
+   * by TreePieces on this PE, although they may have been
+   * fetched from a remote source.
+   */
   Remote,
+  // Same as above, but this is a leaf
   RemoteBucket,
+  // Same as above, but this is a leaf with no particles in it
   RemoteEmptyBucket
 };
 
@@ -28,19 +55,37 @@ enum NodeType {
 extern string NodeTypeString[];
 #endif
 
+/*
+ * These are "core" fields that would be present in any distributed memory 
+ * tree data structure.
+ * The part that can be customized for each algorithm is the 'data' field
+ * of the Node data structure.
+ */
 struct NodeCore {
   Key key;
   NodeType type;
 
   int depth;
+  // Pointer to particles underneath this node (valid only if Internal)
   Particle *particleStart;
+  // Number of particles underneath this node
   int numParticles;
+  // Number of children nodes 
   int numChildren;
 
-  // tps [ownerStart,ownerEnd] own this node
+  // TreePieces [ownerStart,ownerEnd] own this node
   int ownerStart;
   int ownerEnd;
 
+  /* 
+   * Was this node fetched from a remote source during traversal?
+   * This check is needed to distinguish those nodes that were allocated
+   * during the building of tree, from those that were received
+   * as responses to requests for remote data. The distinction is important
+   * because the former are allocated as arrays of size BRANCH_FACTOR (except
+   * for the root, which is always allocated as a lone node), whereas the
+   * latter are received as messages containing entire subtrees of nodes.
+   */
   bool cached;
 
   NodeCore(){
@@ -63,6 +108,11 @@ struct NodeCore {
   }
 };
 
+/*
+ * Actual Node data structure. Has a core (described above)
+ * and pointer to children (all BRANCH_FACTOR of which are allocated
+ * in a single array) and the node's parent (NULL for root). 
+ */
 template<typename T>
 class Node {
 
@@ -71,9 +121,21 @@ class Node {
   Node<T> *children;
   Node<T> *parent;
 
+  /* 
+   * Used during tree building to ascertain whether the moments of all
+   * of this node's children have been computed (or received). Then,
+   * the moments of this node may be computed.
+   */
   int numChildrenMomentsReady;
 
   public:
+  /*
+   * Data specific to the kind of tree we have built.
+   * For a decomposition tree, this template would be instantiated 
+   * with the NodeDescriptor type, whereas for the tree used during
+   * traversal and force computation, it is of type ForceData, and
+   * contains the MultipoleMoments of the node.
+   */
   T data;
 
   Node(Key k, int d, Particle *p, int np, Node<T> *par=NULL) : 
@@ -88,6 +150,7 @@ class Node {
   {
   }
 
+  // What is the key of the first particle that can be enclosed by this node?
   static Key getParticleLevelKey(const Node<T> *node){
     Key k = node->getKey();
     int depth = node->getDepth();
@@ -95,6 +158,7 @@ class Node {
     return (k<<(TREE_KEY_BITS-(LOG_BRANCH_FACTOR*depth+1)));
   }
 
+  // ditto for last particle enclosed.
   static Key getLastParticleLevelKey(const Node<T> *node){
     Key k = node->getKey();
     int depth = node->getDepth();
@@ -107,10 +171,15 @@ class Node {
     return k;
   }
 
+  // Find depth of node from position of prepended '1' bit in node
   static int getDepthFromKey(Key k){
     int nUsedBits = mssb64_pos(k);
     return (nUsedBits/LOG_BRANCH_FACTOR);
   }
+
+  /*
+   * Various get/set methods.
+   */
 
   int getNumChildren() const {
     return core.numChildren;
@@ -184,6 +253,13 @@ class Node {
   }
 #endif
 
+  /*
+   * refine() is invoked on a node that is to be partitioned. 
+   * In this method, the node's children are allocated and 
+   * initialized. Also, the particles of the parent node are
+   * distributed among the children, as decided by the keys
+   * of the children. 
+   */
   void refine(){
     CkAssert(core.numChildren == 0);
     int numRankBits = LOG_BRANCH_FACTOR;
@@ -191,6 +267,7 @@ class Node {
     CkAssert(depth >= 0);
     CkAssert(depth < ((TREE_KEY_BITS-1)/numRankBits));
 
+    // Allocate children
     core.numChildren = BRANCH_FACTOR;
     children = new Node<T>[BRANCH_FACTOR];
 
@@ -205,6 +282,15 @@ class Node {
     Key childKey = (myKey << numRankBits);
     int childDepth = depth+1;
 
+    /*
+     * findSplitters() distributes the parent's particles among the 
+     * children. This is done by considering the Key of each child
+     * and finding the first of the parent's particles whose Key is
+     * geq to that of the child. This gives the starting point for the
+     * particles of the child. The end is given by the start of the next
+     * child. We are able to do this in-place partitioning because 
+     * particles are assumed to have been sorted beforehand.
+     */
     findSplitters(particles,start,end,splitters,childKey,childDepth);
 
     for(int i = 0; i < BRANCH_FACTOR; i++){
@@ -213,8 +299,10 @@ class Node {
     }
   }
 
+  // Initialize various fields of child based on those of parent.
   void initChild(int i, int *splitters, Key childKey, int childDepth){
     Node<T> *child = children+i;
+    // The splitters array was filled in by findSplitters()
     int childPartStart = splitters[i]; 
     int childPartEnd = splitters[i+1]; 
     int childNumParticles = childPartEnd-childPartStart;
@@ -224,8 +312,6 @@ class Node {
     child->setDepth(childDepth);
     child->setParent(this);
 
-    //CkPrintf("(%d) child %lu splitteridx %d\n", CkMyPe(), child->getKey(), childPartStart);
-
     // findSplitters distributed particles over
     // different children
     child->core.particleStart = core.particleStart+childPartStart;
@@ -233,11 +319,29 @@ class Node {
     child->core.numChildren = 0;
   }
 
+  /* 
+   * serialize():
+   * Called when the subtree under a node has to be communicated to a 
+   * requestor DM. The method is recursive. Explanations of the code are
+   * provided below.
+   */
   void serialize(Node<T> *placeInBuf, Node<T> *&emptyBuf, int subtreeDepth){
+    /* 
+     * serialize() works by having each node copy its children into
+     * the serialization buffer, and invoking serialize() on them
+     * recursively. The procedure terminates when the entire subtree 
+     * up to a certain depth under the original node has been placed
+     * in the buffer, or if a node has no children.
+     * 'emptybuf' points to that part of the array that hasn't yet 
+     * been filled with nodes. This is where the children of this 
+     * node will be placed. 'placeInBuf' is the pointer to the 
+     * serialized copy of this node.
+     */
     CmiUInt8 childOffset = emptyBuf-placeInBuf;
+    // We have reached the depth cutoff for the subtree
     if(subtreeDepth == 0){
       // if subtreeDepth is 0 and placeInBuf is NULL, then
-      // we have been asked to place all of the root's
+      // we have been asked to place all of the node's
       // successors up to depth 0, but not the root itself.
       // this is a nonsensical request.
       CkAssert(placeInBuf != NULL);
@@ -246,13 +350,19 @@ class Node {
       return;
     }
 
+    // PlaceInBuf is NULL for the root, since we don't want
+    // to place the root itself in the serialization buffer, 
+    // only the subtree under it, starting from its children.
     if(placeInBuf != NULL){
-      // we exclude the root from the buffer 
+      // We exclude the root from the buffer 
       placeInBuf->setChildren((Node<T>*)childOffset,getNumChildren());
     }
 
-    // copy children into empty portion of buffer
+    // Copy children into empty portion of buffer
     memcpy(emptyBuf,getChildren(),sizeof(Node<T>)*getNumChildren());
+    // Set the parent pointers (in integer offsets) of your children
+    // We don't do this for the serialized children of the subtree root, 
+    // since they are being transported to a different PE.
     if(placeInBuf != NULL){
       Node<T> *childInBuf = emptyBuf;
       for(int i = 0; i < getNumChildren(); i++){
@@ -260,37 +370,70 @@ class Node {
         childInBuf++;
       }
     }
-    // mark the start of children
+    // This is where I put the serialized copy of my first child
     placeInBuf = emptyBuf;
-    // we have used up a bit of the empty space in the buffer
+    // we have used up some of the empty space in the buffer
     emptyBuf += getNumChildren();
 
     // invoke serialize on each of the children in turn
     for(int i = 0; i < getNumChildren(); i++){
       getChild(i)->serialize(placeInBuf,emptyBuf,subtreeDepth-1);
-      // move to space reserved for next child in buffer
+      // move placeInBuf to point to the serialized copy of the next
+      // child; emptyBuf is updated by serialize() recursively
       placeInBuf++;
     }
   }
 
+  /*
+   * This is the reverse of the serialize() procedure; it is invoked
+   * on the DM (PE) that previously sent a request to the owner for 
+   * (the subtree under) a node. Its purpose is to translate the 
+   * integer offsets encoding parent-child relationships into normal
+   * pointers so that these nodes can be traversed as a pointer-linked
+   * tree data structure.
+   * Here, 'start' points to the buffer of serialized nodes, and 'nn' 
+   * is the number of nodes in that buffer. The method is invoked on
+   * the parent on the requesting DM, whose children were requested 
+   * in the first place. 
+   */
   void deserialize(Node<T> *start, int nn){
+    /*
+     * Set the children of this node to be the first BRANCH_FACTOR 
+       nodes in the buffer. This works because on the owner PE, the
+       corresponding root of this subtree would have copied its 
+       children into the serialization buffer first.
+     */
     setChildren(start,BRANCH_FACTOR); 
     Node<T> *buf = start;
+    // Go through all the nodes in the buffer and update their pointers
     for(int i = 0; i < nn; i++){
+      // Set this node to be the parent of the first BRANCH_FACTOR nodes
       if(i < BRANCH_FACTOR){
         buf->setParent(this);
       }
       else{
         CmiUInt8 parentOffset = (CmiUInt8)(buf->getParent());
+        // Add base address of this buffer to the relative offset
+        // that was saved during serialization. This gives the absolute
+        // address of the parent.
         buf->setParent(buf+parentOffset);
       }
+      // Similar relative-to-absolute address conversion for pointers to
+      // children 
       CmiUInt8 childrenOffset = (CmiUInt8)(buf->getChildren());
       buf->setChildren(buf+childrenOffset,buf->getNumChildren());
 
+      // There are no particles under this remotely fetched node yet.
       buf->setParticles(NULL,0);
+      // Translate type from what it was on the owner PE. e.g. a node that
+      // was Internal on the owner PE becomes a Remote here, a Bucket becomes
+      // a RemoteBucket, etc.
       buf->setType(makeRemote(buf->getType()));
+      // This node has been fetched from a remote location, and is part of an
+      // array that includes not just its siblings, other nodes too.
       buf->setCached();
 
+      // Move to next node in serialized buffer.
       buf++;
     }
   }
@@ -298,6 +441,9 @@ class Node {
   void setCached(){ core.cached = true; }
   bool isCached(){ return core.cached; }
 
+  /*
+   * Translate the type of a remotely fetched node.
+   */
   static NodeType makeRemote(NodeType type){
     switch(type){
       case Boundary : 
@@ -313,6 +459,10 @@ class Node {
     }
   }
 
+  /*
+   * Used during tree building. Calculate the moments of a node
+   * from the moments of its children. 
+   */
   void getMomentsFromChildren(){
     OrientedBox<Real> &bb = data.box;
     Real &mass = data.moments.totalMass;
@@ -320,12 +470,18 @@ class Node {
 
     for(Node<ForceData> *c = getChildren(); c != getChildren()+getNumChildren(); c++){
       MultipoleMoments &childMoments = c->data.moments;
+      // My mass is the sum of the mass of each child
       mass += childMoments.totalMass;
+      // My center of mass is the weighted average of the centers of mass 
+      // of my children
       cm += childMoments.totalMass*childMoments.cm;
+      // Grow my bounding box to enclose that of my child.
       bb.grow(c->data.box);
     }
     if(mass > 0.0) cm /= mass;
 
+    // Radius: distance between center of mass and the corner that is
+    // farthest from it.
     Vector3D<Real> delta1 = data.moments.cm - data.box.lesser_corner;	
     Vector3D<Real> delta2 = data.box.greater_corner - data.moments.cm;
     delta1.x = (delta1.x > delta2.x ? delta1.x : delta2.x);
@@ -334,6 +490,10 @@ class Node {
     data.moments.rsq = delta1.lengthSquared();
   }
 
+  /*
+   * If this node is a leaf, obtain its moments from 
+   * the particles that it encloses.
+   */
   void getMomentsFromParticles(){
     OrientedBox<Real> &bb = data.box;
     Real &mass = data.moments.totalMass;
@@ -342,12 +502,14 @@ class Node {
     Particle *p;
     for(p = getParticles(); p != getParticles()+getNumParticles(); p++) {
       mass += p->mass;
+      // Center of mass is weighted avg of particles' centers of mass
       cm += p->mass*p->position;
       bb.grow(p->position);
     }
     if(mass > 0.0) cm /= mass;
 
     Real d;
+    // Radius: distance between center of mass and particle farthest from it
     data.moments.rsq = 0;
     for(p = getParticles(); p != getParticles()+getNumParticles(); p++) {
       d = (cm - p->position).lengthSquared();
@@ -357,6 +519,10 @@ class Node {
   }
 };
 
+/*
+ * When a DM requests the information of a Remote node during tree building, 
+ * it only needs certain fields, as embodied by this structure.
+ */
 struct MomentsExchangeStruct {
   MultipoleMoments moments;
   OrientedBox<Real> box;
