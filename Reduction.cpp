0fdc4203f0cbc549e90531cb362a3d6f4a2d02da
Pritish Jetley <pjetley2@illinois.edu>
2011-10-23 23:05:50 -0500

    Reduction.cpp doc

--- Reduction.cpp
+++ Reduction.cpp
@@ -1,3 +1,11 @@
+/*
+ * CharmBH: Reductions.cpp
+ * Customized reduction operations used in the program. 
+ * The serial operations (grow(), operator+=, etc.) for each reduced 
+ * data type (BoundingBox,NodeDescriptor, etc.) are defined in 
+ * Descriptor.h. 
+ */
+
 #include "charm++.h"
 #include "OrientedBox.h"
 #include "defines.h"
@@ -7,6 +15,11 @@ CkReduction::reducerType BoundingBoxGrowReductionType;
 CkReduction::reducerType NodeDescriptorReductionType;
 CkReduction::reducerType DtReductionType;
 
+/*
+ * Reduction type for constructing the bounding box of the entire
+ * simulation universe. Each PE contributes the bounding box of its
+ * particles.
+ */
 CkReductionMsg *BoundingBoxGrowReduction(int nmsgs, CkReductionMsg **msgs){
   BoundingBox &bb = *((BoundingBox *) msgs[0]->getData());
   for(int i = 1; i < nmsgs; i++){
@@ -17,6 +30,13 @@ CkReductionMsg *BoundingBoxGrowReduction(int nmsgs, CkReductionMsg **msgs){
   return CkReductionMsg::buildNew(sizeof(BoundingBox),&bb);
 }
 
+/*
+ * Reduction type for computing the total number of particles underneath 
+ * active nodes during decomposition. Also computes the smallest and 
+ * largest keys of particles under active nodes, across all PEs.
+ * Each PE contributes the number of its particles that fall under an 
+ * active node, as well as the smallest and largest keys among them.
+ */
 CkReductionMsg *NodeDescriptorReduction(int nmsgs, CkReductionMsg **msgs){
   int numElements = (msgs[0]->getSize()/sizeof(NodeDescriptor));
 
@@ -30,6 +50,12 @@ CkReductionMsg *NodeDescriptorReduction(int nmsgs, CkReductionMsg **msgs){
   return CkReductionMsg::buildNew(msgs[0]->getSize(),msgs[0]->getData());
 }
 
+/*
+ * Reduction type for basic statistics instrumented during the course of the
+ * tree traversal on each PE. These are counts of particle-node interactions,
+ * particle-particle interactions and number of calls to the opening criterion
+ * function (gravity.h)
+ */
 CkReductionMsg *DtReduction(int nmsgs, CkReductionMsg **msgs){
   int numElements = (msgs[0]->getSize()/sizeof(DtReductionStruct));
 
@@ -43,6 +69,10 @@ CkReductionMsg *DtReduction(int nmsgs, CkReductionMsg **msgs){
   return CkReductionMsg::buildNew(msgs[0]->getSize(),msgs[0]->getData());
 }
 
+/*
+ * Function called on each processor (initproc) to register the customized reduction
+ * types with Charm++.
+ */
 void registerReducers(){
   BoundingBoxGrowReductionType = CkReduction::addReducer(BoundingBoxGrowReduction);
   NodeDescriptorReductionType = CkReduction::addReducer(NodeDescriptorReduction);
