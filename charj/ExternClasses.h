b46ae7ad574f5721190af7157eb48995609bf131
Pritish Jetley <pjetley2@illinois.edu>
2012-05-25 19:43:20 -0500

    shift Node into C++ from charj

--- charj/ExternClasses.h
+++ charj/ExternClasses.h
@@ -7,3 +7,541 @@ struct TreeRootContainer {
     pe(pe_)
   {}
 };
+
+/*
+ * ForceData:
+ * Has fields to store the moments of a tree node. This includes the 
+ * center of mass, total mass and radius of node.
+ */
+struct ForceData {
+  OrientedBox<double> box;
+  MultipoleMoments moments;
+
+  ForceData() : box(), moments()
+  {
+  }
+};
+
+/*
+ * CharmBH: Node.h
+ * Tree node data structure. Used in spatial partitioning of particles.
+ * Each node has a key, type, depth, pointer to the particles underneath
+ * it (valid only for "Internal" nodes, i.e. those nodes for which all
+ * enclosed particles are hosted on the same PE as the node itself.), 
+ * and child nodes. 
+ * The Node data structure also tracks the owners of a node, i.e. 
+ * which TreePieces have particles under that node. So, for example,
+ * the root is owned collectively by all the TreePieces that have particles.
+ */
+
+// Type of node
+enum NodeType {
+  Invalid = 0,
+
+  // All particles under the node are on this PE
+  Internal,
+  // This is a leaf
+  Bucket,
+  // This is a leaf with no particles in it
+  EmptyBucket,
+
+  /* 
+   * Some of the particles underneath this node are
+   * owned by TreePieces that are not on this PE
+  */
+  Boundary,
+
+  /*
+   * None of the particles underneath this node are owned 
+   * by TreePieces on this PE, although they may have been
+   * fetched from a remote source.
+   */
+  Remote,
+  // Same as above, but this is a leaf
+  RemoteBucket,
+  // Same as above, but this is a leaf with no particles in it
+  RemoteEmptyBucket
+};
+
+/*
+ * Actual Node data structure. Has a core (described above)
+ * and pointer to children (all BRANCH_FACTOR of which are allocated
+ * in a single array) and the node's parent (NULL for root). 
+ */
+class Node{
+  Key key;
+  NodeType type;
+
+  int depth;
+  // Pointer to particles underneath this node (valid only if Internal)
+  CkVec<Particle> *particles;
+  int startParticleIdx;
+  int numParticles;
+
+  CkVec<Node*> children;
+
+  // TreePieces [ownerStart,ownerEnd) own this node
+  int ownerStart;
+  int ownerEnd;
+
+  /* 
+   * Was this node fetched from a remote source during traversal?
+   * This check is needed to distinguish those nodes that were allocated
+   * during the building of tree, from those that were received
+   * as responses to requests for remote data. The distinction is important
+   * because the former are allocated as arrays of size BRANCH_FACTOR (except
+   * for the root, which is always allocated as a lone node), whereas the
+   * latter are received as messages containing entire subtrees of nodes.
+   */
+  bool cached;
+
+  Node *parent;
+
+  /* 
+   * Used during tree building to ascertain whether the moments of all
+   * of this node's children have been computed (or received). Then,
+   * the moments of this node may be computed.
+   */
+  int numChildrenMomentsReady;
+
+  public:
+  /*
+   * Data specific to the kind of tree we have built.
+   * For a decomposition tree, this template would be instantiated 
+   * with the NodeDescriptor type, whereas for the tree used during
+   * traversal and force computation, it is of type ForceData, and
+   * contains the MultipoleMoments of the node.
+   */
+  ForceData data;
+
+  Node(){
+    type = Invalid;
+    depth = -1;
+    startParticleIndex = -1;
+    numChildren = 0;
+
+    ownerStart = -1;
+    ownerEnd = -1;
+
+    cached = false;
+    parent = NULL;
+
+    numChildrenMomentsReady = 0;
+  }
+
+  Node(Key k, int d, CkVec<Particle> *p, int start, int np, Node par) {
+    key = k;
+    depth = d;
+    particles = p;
+    numParticles = np;
+    startParticleIdx = start;
+    type = Invalid;
+    parent = par;
+
+    numChildrenMomentsReady = 0;
+  }
+
+  // What is the key of the first particle that can be enclosed by this node?
+  static Key getParticleLevelKey(Node *node){
+    Key k = node->getKey();
+    int depth = node->getDepth();
+
+    return (k<<(TREE_KEY_BITS-(LOG_BRANCH_FACTOR*depth+1)));
+  }
+
+  // ditto for last particle enclosed.
+  static Key getLastParticleLevelKey(Node *node){
+    Key k = node->getKey();
+    int depth = node->getDepth();
+
+    int nshift = TREE_KEY_BITS-(LOG_BRANCH_FACTOR*depth+1);
+    k <<= nshift;
+    Key mask = (((Key)1) << nshift);
+    mask--;
+    k |= mask;
+    return k;
+  }
+
+  // Find depth of node from position of prepended '1' bit in node
+  static int getDepthFromKey(Key k){
+    int nUsedBits = mssb64_pos(k);
+    return (nUsedBits/LOG_BRANCH_FACTOR);
+  }
+
+  static int completeTreeSize(int levels){
+    return numLeaves(levels+1)-1;
+  }
+
+  static int numLeaves(int levels){
+    return (1<<(LOG_BRANCH_FACTOR*levels));
+  }
+  /*
+   * Various get/set methods.
+   */
+
+  int getNumChildren() { return children.size(); }
+  void addChild(Node *node){ children.push_back(node); }
+  void setChild(int i, Node *node){ children[i] = node; }
+  Node *getChild(int i) { return children[i]; }
+
+  int getNumParticles() { return numParticles; }
+  CkVec<Particle> *getParticles() { return particles; }
+
+  Key getKey() { return key; }
+  int getDepth() { return depth; }
+  int getOwnerStart() { return ownerStart; }
+  int getOwnerEnd() { return ownerEnd; }
+  NodeType getType() { return type; }
+  bool isInternal() {
+    return (type == EmptyBucket || type == Bucket || type == Internal);
+  }
+  Node getParent() { return parent; }
+  void setKey(Key k){ key = k; }
+  void setDepth(int d){ depth = d; }
+  void setType(NodeType t){ type = t; }
+  void setParent(Node *par){ parent = par; }
+
+  void setOwners(int lb, int ub){
+    ownerStart = lb;
+    ownerEnd = ub;
+  }
+
+  void setOwnerStart(int lb){ ownerStart = lb; }
+  void setOwnerEnd(int ub){ ownerEnd = ub; }
+
+  void setParticles(CkVec<Particle> *p, int start, int n){
+    startParticleIdx = start
+    particles = p;
+    numParticles = n;
+  }
+
+  void setNumParticles(int n){
+    numParticles = n;
+  }
+
+  void childMomentsReady(){ numChildrenMomentsReady++; }
+  int getNumChildrenMomentsReady() { return numChildrenMomentsReady; }
+  bool allChildrenMomentsReady() { return numChildrenMomentsReady == getNumChildren(); }
+  void setChildrenMomentsReady() { numChildrenMomentsReady = getNumChildren(); }
+
+  /*
+   * refine() is invoked on a node that is to be partitioned. 
+   * In this method, the node's children are allocated and 
+   * initialized. Also, the particles of the parent node are
+   * distributed among the children, as decided by the keys
+   * of the children. 
+   */
+    
+  void refine(){
+    refine(1);
+  }
+
+  void refine(int levels){
+    if(levels == 0) return;
+
+    CkAssert(getNumChildren() == 0);
+    int numRankBits = LOG_BRANCH_FACTOR;
+    int depth = getDepth();
+    CkAssert(depth >= 0);
+    CkAssert(depth < ((TREE_KEY_BITS-1)/numRankBits));
+
+    // Allocate children
+    numChildren = BRANCH_FACTOR;
+    children.resize(BRANCH_FACTOR);
+
+    Key myKey = getKey();
+
+    int start = startParticleIdx;
+    int end = start+getNumParticles();
+    CkVec<Particle> *p = getParticles();
+
+    int splitters[BRANCH_FACTOR+1];
+
+    Key childKey = (myKey << numRankBits);
+    int childDepth = depth+1;
+
+    /*
+     * findSplitters() distributes the parent's particles among the 
+     * children. This is done by considering the Key of each child
+     * and finding the first of the parent's particles whose Key is
+     * geq to that of the child. This gives the starting point for the
+     * particles of the child. The end is given by the start of the next
+     * child. We are able to do this in-place partitioning because 
+     * particles are assumed to have been sorted beforehand.
+     */
+    findSplitters(p,start,end,splitters,childKey,childDepth);
+
+    for(int i = 0; i < BRANCH_FACTOR; i++){
+      setChild(i,new Node());
+      initChild(i,splitters,childKey,childDepth);
+      getChild(i)->refine(levels-1);
+      childKey++;
+    }
+
+    delete splitters;
+    
+  }
+
+  // this version is used during decomposition, 
+  // in order to maintain a list of "active" nodes
+  // for the next iteration of histogramming
+  void refine(CkVec<int> &counts, CkVec<PossiblySplitNode> &active){
+    refine(counts,active,1);
+  }
+
+  void refine(CkVec<int> &counts, CkVec<PossiblySplitNode> &active, int levels){
+    if(levels == 0){
+      active.push_back(PossiblySplitNode(this));
+      counts.push_back(getNumParticles());
+      return;
+    }
+
+    CkAssert(getNumChildren() == 0);
+    int numRankBits = LOG_BRANCH_FACTOR;
+    int depth = getDepth();
+    CkAssert(depth >= 0);
+    CkAssert(depth < ((TREE_KEY_BITS-1)/numRankBits));
+
+    // Allocate children
+    children.resize(BRANCH_FACTOR);
+
+    Key myKey = getKey();
+
+    int start = startParticleIdx;
+    int end = start+getNumParticles();
+    CkVec<Particle> *p = getParticles();
+    int splitters[BRANCH_FACTOR+1];
+
+    Key childKey = (myKey << numRankBits);
+    int childDepth = depth+1;
+
+    /*
+     * findSplitters() distributes the parent's particles among the 
+     * children. This is done by considering the Key of each child
+     * and finding the first of the parent's particles whose Key is
+     * geq to that of the child. This gives the starting point for the
+     * particles of the child. The end is given by the start of the next
+     * child. We are able to do this in-place partitioning because 
+     * particles are assumed to have been sorted beforehand.
+     */
+    findSplitters(p,start,end,splitters,childKey,childDepth);
+
+    for(int i = 0; i < BRANCH_FACTOR; i++){
+      setChild(i,new Node());
+      initChild(i,splitters,childKey,childDepth);
+      getChild(i)->refine(counts,active,levels-1);
+      childKey++;
+    }
+
+    delete splitters;
+  }
+
+  void reuseRefine(){
+    int start = 0;
+    int end = getNumParticles();
+    Array<Particle> p = getParticles();
+
+    int numRankBits = LOG_BRANCH_FACTOR;
+    Key childKey = (getKey() << numRankBits);
+    int childDepth = getDepth()+1;
+
+    Array<int> splitters = new Array<int>(BRANCH_FACTOR+1);
+    findSplitters(p,start,end,splitters,childKey,childDepth);
+    Array<Node> child = getChildren();
+    for(int i = 0; i < BRANCH_FACTOR; i++){
+      start = splitters[i];
+      end = splitters[i+1];
+      child.setParticles(p,start,end-start);
+      child++; 
+    }
+  }
+
+  // Initialize various fields of child based on those of parent.
+  void initChild(int i, int *splitters, Key childKey, int childDepth){
+    Node *child = getChild(i);
+    // The splitters array was filled in by findSplitters()
+    int childPartStart = splitters[i]; 
+    int childPartEnd = splitters[i+1]; 
+    int childNumParticles = childPartEnd-childPartStart;
+
+    // set child's key and depth
+    child.setKey(childKey);
+    child.setDepth(childDepth);
+    child.setParent(this);
+
+    // findSplitters distributed particles over
+    // different children
+    child.setParticles(getParticles(),childPartStart,childNumParticles);
+  }
+
+  NodeReplyMsg serialize(int subtreeDepth){
+    NodeReplyMsg msg = new NodeReplyMsg(getKey());
+    for(int i = 0; i < BRANCH_FACTOR; i++){
+      msg.roots[i] = getChild(i).copy(subtreeDepth);
+    }
+  }
+
+  Node copy(int depth){
+    Node root = new Node(this);
+    if(depth > 0 && getNumChildren() > 0){
+      root.setChildren(new Array<Node>(BRANCH_FACTOR),BRANCH_FACTOR);
+      for(int i = 0; i < getNumChildren(); i++){
+        root.getChild(i) = copy(getChild(i),depth-1);
+      }
+    }
+    else{
+      root.setChildren(null,0);
+    }
+    return root;
+  }
+
+  void setCached(){ setCached(true); }
+  void setCached(boolean val){ cached = val; }
+  bool isCached(){ return cached; }
+
+  /*
+   * Translate the type of a remotely fetched node.
+   */
+  static NodeType makeRemote(NodeType type){
+    switch(type){
+      case Boundary : 
+      case Internal : return Remote;
+      case Bucket : return RemoteBucket;
+      case EmptyBucket : return RemoteEmptyBucket;
+
+      case Remote :
+      case RemoteBucket :
+      case RemoteEmptyBucket : return type;
+
+      default: CkAbort("bad type!\n");
+    }
+    return Invalid;
+  }
+
+  /*
+   * Used during tree building. Calculate the moments of a node
+   * from the moments of its children. 
+   */
+  void getMomentsFromChildren(){
+    OrientedBox<double> bb = data.box;
+    Real mass = data.moments.totalMass;
+    Vector3D<Real> &cm = data.moments.cm;
+
+    for(int i = 0; i < getNumChildren(); i++){
+      Node child = getChild(i);
+      MultipoleMoments childMoments = child.data.moments;
+      // My mass is the sum of the mass of each child
+      mass += childMoments.totalMass;
+      // My center of mass is the weighted average of the centers of mass 
+      // of my children
+      // FIXME += allowed?
+      cm += childMoments.totalMass*childMoments.cm;
+      // Grow my bounding box to enclose that of my child.
+      bb.grow(child.data.box);
+    }
+    if(mass > 0.0) cm /= mass;
+
+    // Radius: distance between center of mass and the corner that is
+    // farthest from it.
+    // FIXME 
+    Vector3D<Real> delta1 = data.moments.cm - data.box.lesser_corner;	
+    Vector3D<Real> delta2 = data.box.greater_corner - data.moments.cm;
+    delta1.x = (delta1.x > delta2.x ? delta1.x : delta2.x);
+    delta1.y = (delta1.y > delta2.y ? delta1.y : delta2.y);
+    delta1.z = (delta1.z > delta2.z ? delta1.z : delta2.z);
+    data.moments.rsq = delta1.lengthSquared();
+  }
+
+  /*
+   * If this node is a leaf, obtain its moments from 
+   * the particles that it encloses.
+   */
+  void getMomentsFromParticles(){
+    OrientedBox<double> bb = data.box;
+    Real mass = data.moments.totalMass;
+    Vector3D<Real> cm = data.moments.cm;
+
+    Array<Particle> p = getParticles();
+    for(int i = 0; i < getNumParticles(); i++){
+      mass += p.mass;
+      // Center of mass is weighted avg of particles' centers of mass
+      cm += p.mass*p.position;
+      bb.grow(p.position);
+    }
+    if(mass > 0.0) cm /= mass;
+
+    Real d;
+    // Radius: distance between center of mass and particle farthest from it
+    data.moments.rsq = 0;
+    for(p = getParticles(); p != getParticles()+getNumParticles(); p++) {
+      d = (cm - position).lengthSquared();
+      if(d > data.moments.rsq) data.moments.rsq = d;
+    }
+
+  }
+
+  void deleteBeneath(){
+    if(getNumChildren() == 0) return;
+    for(int i = 0; i < getNumChildren(); i++){
+      getChild(i).deleteBeneath();
+    }
+    delete getChildren();
+    setChildren(null,-1,0);
+  }
+
+  void reuseTree(){
+    if(getType() == Invalid) return;
+    reset();
+    if(getNumChildren() == 0) return;
+    for(int i = 0; i < getNumChildren(); i++){
+      getChild(i).reuseTree();
+    }
+  }
+
+  void reset(){
+    setType(Invalid);
+    setParticles(null,-1,0);
+    numChildrenMomentsReady = 0;
+    data = T();
+  }
+
+};
+
+/*
+ * When a DM requests the information of a Remote node during tree building, 
+ * it only needs certain fields, as embodied by this structure.
+ */
+class MomentsExchangeStruct {
+  public MultipoleMoments moments = new MultipoleMoments();
+  public OrientedBox<double> box = new OrientedBox<double>();
+  public Key key;
+  public NodeType type = Invalid;
+
+  MomentsExchangeStruct(Node node){
+    moments = node.data.moments;
+    box = node.data.box;
+    key = node.getKey()
+    type = node.getType();
+  }
+
+  void pup(PUPer p){
+    p|moments;
+    p|box;
+    p|key;
+    p|type;
+  }
+};
+
+class NodeRequest {
+  Key key;
+  int replyTo;
+
+  NodeRequest() {}
+  NodeRequest(Key _key, int _replyTo){
+    key = _key;
+    replyTo = _replyTo;
+  }
+
+  Key getKey() { return key; }
+  int getReplyTo() { return replyTo; }
+};
--- charj/ExternFunctions.cpp
+++ charj/ExternFunctions.cpp
@@ -16,4 +16,293 @@ void findOrbLB(){
   }
 }
 
+void getNumParticles();
+void setParameters(CkArgMsg *m, Parameters &params){
+  map<string,string> table;
+  params.extractParameters(m->argc, m->argv, table); 
+
+  map<string,string>::iterator it = table.find("in");
+  if(it == table.end()){
+    CkPrintf("[Main] Must specify input particle file:\n");
+    usage();
+    CkAbort("bad command line arguments\n");
+  }
+
+  /* Input file */
+  string filename = params.getsparam("in", table);
+  params.nchars = filename.length();
+  params.filename = new char[params.nchars+1];
+  filename.copy(params.filename,params.nchars);
+  params.filename[params.nchars] = '\0';
+
+  /* Opening angle */
+  params.theta = params.getrparam("theta", DEFAULT_THETA, table);
+  CkPrintf("theta: %f\n", params.theta);
+
+  /* Step time */
+  params.dtime = params.getrparam("dtime", DEFAULT_DTIME, table);
+  CkPrintf("dtime: %f\n", params.dtime);
+
+  /* Half of step time; used in leapfrog "kick" */
+  params.dthf = params.dtime/2.0;
+
+  /* Softening parameter */
+  params.epssq = params.getrparam("eps", DEFAULT_EPS, table);
+  CkPrintf("eps: %f\n", params.epssq);
+  params.epssq = params.epssq*params.epssq;
+
+  params.tolsq = params.theta;
+  CkPrintf("tol: %f\n", params.tolsq);
+  params.tolsq = params.tolsq*params.tolsq;
+
+  /* Particles per chare (number of particles per tree piece) */
+  params.ppc = params.getiparam("ppc", DEFAULT_PPC, table); 
+  CkPrintf("ppc: %d\n", params.ppc);
+
+  params.ppb = params.getiparam("b", DEFAULT_PPB, table);
+  CkPrintf("bucketSize: %d\n", params.ppb);
+
+  /* Number of steps to run the simulation for */
+  params.iterations = params.getiparam("killat", DEFAULT_KILLAT, table);
+  CkPrintf("killat: %d\n", params.iterations);
+
+  /* 
+     When a remote node is requested, the subtree beneath
+     it up to "cacheLineSize" is communicated alongwith the
+     node itself. This helps to coarsen the grain of communication.
+     */
+  params.cacheLineSize = params.getiparam("chunkdepth", DEFAULT_CHUNK_DEPTH, table);
+  CkPrintf("chunkdepth: %d\n", params.cacheLineSize);
+
+  /*
+     When traversing the global tree for each bucket of particles, 
+     do "yield" number of buckets before returning to the scheduler
+     to look for outstanding request messages, etc.
+     */
+  params.yieldPeriod = params.getiparam("yield", DEFAULT_YIELD_PERIOD, table);
+  CkPrintf("yieldPeriod: %d\n", params.yieldPeriod);
+
+  /*
+     Do load balancing every "balancePeriod" iterations.
+     */
+  params.balancePeriod = params.getiparam("balancePeriod", DEFAULT_BALANCE_PERIOD, table);
+  CkPrintf("balancePeriod: %d\n", params.balancePeriod);
+
+  params.doPrintAccel = false;
+  if(table.find("printAccel") != table.end()) params.doPrintAccel = true;
+  CkPrintf("doPrintAccel: %d\n", params.doPrintAccel);
+
+  params.doPrintTree = false;
+  if(table.find("printTree") != table.end()) params.doPrintTree = true;
+  CkPrintf("doPrintTree: %d\n", params.doPrintTree);
+
+  /* Number of steps between decompositions */
+  params.decompPeriod = params.getiparam("decompPeriod", DEFAULT_DECOMP_PERIOD, table); 
+  CkPrintf("decompPeriod: %d\n", params.decompPeriod);
+
+  /* To prevent sending of very large messages after decomposition */
+  params.particleMsgMaxSize = params.getiparam("particleMsgMaxSize", DEFAULT_PARTICLE_MSG_MAX_SIZE, table); 
+  CkPrintf("particleMsgMaxSize: %d\n", params.particleMsgMaxSize);
+
+  /* Decide several bits in one iteration when decomposing */
+  params.decompLevels = params.getiparam("decompLevels", DEFAULT_DECOMP_BITS, table); 
+  CkPrintf("decompLevels: %d\n", params.decompLevels);
+
+
+  /*
+     Use the filename obtained previously to read just the 
+     number of particles from the input file.
+     */
+  getNumParticles();
+
+  /* 
+     Find the appropriate number of tree pieces to generate.
+     */
+  it = table.find("p");
+  /* 
+     Find the appropriate number of tree pieces to generate.
+     */
+  if(it == table.end()){
+    /*
+       If the number of pieces is not specified by the user, 
+       calculate it from the number of particles per chare and
+       the total number of particles.
+       */
+    params.numTreePieces = CkNumPes()*8;
+  }
+  else{
+    /*
+       If the number of pieces is specified by the user, use that.
+       */
+    params.numTreePieces = atoi(it.second.c_str());
+  }
+
+  CkPrintf("tree pieces: %d\n", params.numTreePieces);
+}
+
+void getNumParticles(){
+  CkPrintf("[Main] file %s\n", globalParams.filename);
+
+  string filename(globalParams.filename);
+  Tipsy::TipsyReader r(filename);
+  if(!r.status()) {
+    cerr << CkMyPe() << ": Fatal: Couldn't open tipsy file! " << filename << endl;
+    CkExit();
+    return;
+  }
+
+  Tipsy::header tipsyHeader = r.getHeader();
+  int nTotalParticles = tipsyHeader.nbodies;
+  int nTotalSPH = tipsyHeader.nsph;
+  int nTotalDark = tipsyHeader.ndark;
+  int nTotalStar = tipsyHeader.nstar;
+
+  CkPrintf("[Main] nsph %d ndark %d nstar %d\n", tipsyHeader.nsph, tipsyHeader.ndark, tipsyHeader.nstar);
+}
+
+int getTipsyNBodies(Tipsy::TipsyReader *r){
+  return r->getHeader().nbodies;
+}
+
+void setTipsyDarkParticle(Particle &p, Tipsy::TipsyReader &r){
+  Tipsy::dark_particle dp;
+  CkAssert(r.getNextDarkParticle(dp)); 
+  p.mass = dp.mass;
+  p.position = dp.pos;
+  p.velocity = dp.vel;
+}
+
+void doPrintTree(string name){
+  ostringstream oss;
+  CkPrintf("[%d] printing tree\n", CkMyPe());
+  oss << name << "." << CkMyPe() << "." << iteration << ".dot";
+  ofstream ofs(oss.str().c_str());
+  ofs << "digraph " << name << "_" << CkMyPe() << "_" << iteration << " {" << endl;
+  if(root != NULL) printTree(root,ofs);
+  ofs << "}" << endl;
+  ofs.close();
+}
+
+void printTree(Node nd, ostream os){
+  os << nd.getKey() 
+    << "[label=\""<< nd.getKey() 
+    << "," << nd.getNumParticles() 
+    << "," << nd.getOwnerStart() << ":" << nd.getOwnerEnd() 
+    << "\\n" << nd.data.moments.cm
+    << "\","
+    << "style=\"filled\""
+    << "color=\"" << NodeTypeColor[nd.getType()] << "\""
+    << "]" << endl;
+  //if(nd->getOwnerEnd()-1 == nd->getOwnerStart()) return;
+  for(int i = 0; i < nd.getNumChildren(); i++){
+    Node *child = nd.getChildren()+i;
+    CkAssert(child != NULL);
+    os << nd.getKey() << " -> " << child.getKey() << endl;
+    printTree(child,os);
+  }
+}
+
+struct ForceData;
+class Node;
+
+int log2ceil(int n);
+Key mssb64(Key x);
+int mssb64_pos(Key x);
+
+void findSplitters(CkVec<Particle> *particles, int start, int end, int *splitters, Key childKey, int childDepth);
+
+template<typename KEY_TYPE, typename OBJ_TYPE>
+int binary_search_ge(const KEY_TYPE &check, const OBJ_TYPE *particles, int start, int end){
+  int lo = start;
+  int hi = end;
+  int mid;
+  while(lo < hi){
+    mid = lo+((hi-lo)>>1);
+    if((*particles)[mid] >= check){
+      hi = mid;
+    }
+    else{
+      lo = mid+1;
+    }
+  }
+  return lo;
+}
+
+int log2ceil(int n){
+  int cur = 1;
+  int ret = 0;
+  while(cur < n){
+    cur <<= 1;
+    ret++;
+  }
+  return ret;
+}
+
+int mssb64_pos(Key x) {
+  int n;
+
+  if (x == Key(0)) return -1;
+  n = 0;
+  if (x > Key(0x00000000FFFFFFFF)) {n += 32; x >>= 32;}
+  if (x > Key(0x000000000000FFFF)) {n += 16; x >>= 16;}
+  if (x > Key(0x00000000000000FF)) {n += 8; x >>= 8;}
+  if (x > Key(0x000000000000000F)) {n += 4; x >>= 4;}
+  if (x > Key(0x0000000000000003)) {n += 2; x >>= 2;}
+  if (x > Key(0x0000000000000001)) {n += 1;}
+  return n;
+}
+
+Key mssb64(Key x)
+{
+  x |= (x >> 1);
+  x |= (x >> 2);
+  x |= (x >> 4);
+  x |= (x >> 8);
+  x |= (x >> 16);
+  x |= (x >> 32);
+  return(x & ~(x >> 1));
+}
+
+bool CompareKeys(void *a, void *b){
+  Key *ownerKey = (Key *)a;
+  Key *k = (Key *)b;
+  return (*ownerKey >= *k);
+}
+
+bool CompareParticleToKey(void *a, void *b){
+  Particle *p = (Particle *)a;
+  Key *k = (Key *)b;
+  return (p->key >= *k);
+}
+
+void findSplitters(Particle *particles, int start, int end, int *splitters, Key childKey, int childDepth){
+  int nRankBits = LOG_BRANCH_FACTOR;
+  /*
+  CkPrintf("findSplitters parentKey %lx nRankBits %d startChildKey %lx parentDepth %d childDepth %d\n", 
+      parentKey, 
+      nRankBits, childKey,
+      parentDepth, childDepth);
+  */
+  // particles of first child always begin at index 0
+  splitters[0] = start;
+  // for all other children, set the beginning of each 
+  // (and, hence, the end of the previous child)
+  for(int i = 1; i < BRANCH_FACTOR; i++){
+    childKey++;
+    Key testKey = (childKey << (TREE_KEY_BITS-(nRankBits*childDepth+1))); 
+    //CkPrintf("(%d) findSplitters [%d - %d] start %lx end %lx test %lx\n", CkMyPe(), start, end-1, particles[start].key, particles[end-1].key, testKey);
+    int firstGEIdx = binary_search_ge<Key,Particle>(testKey, particles, start, end); 
+    splitters[i] = firstGEIdx;
+    start = firstGEIdx;
+  }
+  // end of the last child is always at index end 
+  // (which is one position past the last particle)
+  splitters[BRANCH_FACTOR] = end;
+}
+
+ExternalParticle &ExternalParticle::operator=(const Particle &p){
+  this->position = p.position;
+  this->mass = p.mass;
+  return *this;
+}
 
