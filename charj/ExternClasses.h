9b61235df5996c239fb09c40723ff4da84cbe537
Pritish Jetley <pjetley2@illinois.edu>
2012-05-27 19:51:05 -0500

    Request data delivery code

--- charj/ExternClasses.h
+++ charj/ExternClasses.h
@@ -399,7 +399,44 @@ struct NodeCacheLine {
     sent = false;
   }
 
-  void deliver();
+  /*
+   * When the subtree under a requested node is obtained on the PE, find the 
+   * workers that requested it, and perform a traversal on this subtree
+   * for each one. Note that the worker may be in the midst of a traversal for
+   * a different bucket from the one that generated this request. Therefore,
+   * we must save and restore its current bucket through get/setContext.
+   */
+  void deliver(){
+    for(int i = 0; i < requestors.length(); i++){
+      Requestor &req = requestors[i];
+      CutoffWorker *worker = req.worker;
+
+      // Save a pointer to the current bucket of the worker.
+      Node *saveContext = worker->getContext();
+      // Replace it with the one that generated the request
+      // for this subtree
+      worker->setContext(req.context);
+
+      // Recall the kind of traversal the worker was performing
+      // when it generated the request... 
+      Traversal *traversal = req.traversal; 
+      // And a pointer to its current book-keeping state
+      State *state = req.state;
+      // For each child of the parent whose children were requested...
+      for(int j = 0; j < msg->getNumSubtreeRoots(); j++){
+        // perform a top-down traversal of this subtree.
+        traversal->topDownTraversal(msg->getSubtreeRoot(j),worker,state);
+      }
+
+      // Restore the current bucket of the worker
+      worker->setContext(saveContext);
+      // Check whether this traversal has completed.
+      state->decrPending();
+      if(state->complete()) worker->done();
+    }
+    requestors.clear();
+  }
+
 
   void free(){
       CkAssert(sent);
@@ -439,7 +476,56 @@ struct ParticleCacheLine {
     sent = false;
   }
 
-  void deliver();
+  /*
+   * When remote particles are received, we must supply them to all workers
+   * on this PE that requested them. These workers will then use them for
+   * computations that couldn't previously perform due to missing data.
+   */
+  void deliver(){
+    // For each requestor of this set of particles
+    for(int i = 0; i < requestors.length(); i++){
+      Requestor &req = requestors[i];
+      // Which worker requested these particles?
+      CutoffWorker *worker = req.worker;
+
+      /*
+       * Workers maintain the current local bucket for which they
+       * are performing the traversal (either remote or local). 
+       * Currently, the worker could be in the middle of a traversal
+       * for some other local bucket. We save the pointer to this 
+       * local bucket here, and replace it with the one which had caused
+       * the worker to request these particles in the first place.
+       */
+      Node *saveContext = worker->getContext();
+      worker->setContext(req.context);
+
+      // so that the worker may do some book-keeping, if required 
+      worker->beforeParticleForces(parent->getKey());
+      for(int j = 0; j < msg->getNumParticles(); j++){ 
+        // supply each particle in the bucket to the worker
+        worker->work(msg->getParticle(j));
+      }
+      // Tell the worker that it will receive no more particles from this
+      // bucket
+      worker->bucketDone(parent->getKey());
+
+      // Restore the current bucket of the worker, in case
+      // it is in the middle of some other traversal.
+      worker->setContext(saveContext);
+
+      // Update the corresponding state to reflect the fact that a previously
+      // outstanding remote data request is now complete.
+      State *state = req.state;
+      state->decrPending();
+      // If there are no more outstanding data requests for the 
+      // traversal, we are done with it.
+      if(state->complete()) worker->done();
+    }
+
+    // No more requestors of this bucket to keep track of 
+    requestors.clear();
+  }
+
 
   void free(){
       CkAssert(sent);
